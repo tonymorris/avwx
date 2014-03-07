3b1e104a9eeb8e81b83bbf2b41d67ef2e8db830d
Hans-Christian Esperer <hc@hcesperer.org>
2014-03-07 14:32:52 +0100

    Make parseWX read METARs from the command line

--- src/ParseWX.hs
+++ src/ParseWX.hs
@@ -5,7 +5,6 @@ import Data.Text (pack)
 
 main :: IO ()
 main = do
-        putStrLn "Enter METAR<ENTER>"
         print . parseWeather . pack $ "METAR EDDF 062020Z 05003KT CAVOK 04/02 Q1029 NOSIG="
         print . parseWeather . pack $ "METAR EDFE 061720Z 04002KT 8000 NSC 09/01 Q1028="
         print . parseWeather . pack $ "METAR EDFE 061720Z 04002KT 8000 3000NE NSC 09/01 Q1028 TEMPO +TSRA="
@@ -18,5 +17,9 @@ main = do
         print . parseWeather . pack $ "METAR EDFE 061720Z 04002KT CAVOK 09/01 Q1028 PROB30 BECMG +TSRA BR BKN002CB="
         print . parseWeather . pack $ "METAR KORD 071051Z 18004KT 7SM CLR M08/M12 A2999 RMK AO2 SLP164 T10831117 $"
         print . parseWeather . pack $ "METAR KORD 071051Z 18004KT 7SM R08R/590242 CLR M08/M12 A2999 RMK AO2 SLP164 T10831117 $"
-        -- interact $ unlines . map (show . parseWeather . pack) . lines
+        print . parseWeather . pack $ "METAR LIBE 071255Z 35014G26KT 9999 SCT015 07/00 Q1015 RMK BKN SCT080 MON LIB VAL NIL VIS MIN 9999="
+        putStrLn "======================================================================"
+        putStrLn ""
+        putStrLn "Enter METARs, one per line:"
+        interact $ unlines . map (show . parseWeather . pack) . lines
               
